066fcee4cb8d73421b6b2f6c411926e53f248978
Slavick03 <vlad.0.st@yandex.ru>
2023-10-06 19:44:08 +0300

    if else

--- test2.java
+++ test2.java
@@ -1,5 +1,12 @@
 public class test2 {
     public static void main(String[] args) {
-        System.out.println("Hello, World!!!!!!!!");
+
+        int user = 6;
+        if (user == 4){
+            System.out.println(true);
+
+        } else {
+            System.out.println(false);
+        }
     }
 }
